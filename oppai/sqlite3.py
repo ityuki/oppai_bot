24f2b426b28386d635080ce4fc9e1ed2e029608b
Ituki Kirihara <ituki@fc.to>
2021-10-29 21:05:38 +0900

    bugfix

--- oppai/sqlite3.py
+++ oppai/sqlite3.py
@@ -69,7 +69,7 @@ class SQLite3:
             rows = cur.fetchall()
             cur.close()
             if len(rows) != 1:
-                self._sql_execute_idx(dbname,0)
+                self._sql_execute_idx(dbname,conn,0)
                 cur = conn.cursor()
                 cur.execute("insert into meta(meta_key,meta_val) values('version','0');")
                 cur.close()
@@ -89,7 +89,7 @@ class SQLite3:
                 ver_val = ver
                 while ver_val < self.sql['index'][dbname]['']['max_index']:
                     ver_val += 1
-                    self._sql_execute_idx(dbname,ver_val)
+                    self._sql_execute_idx(dbname,conn,ver_val)
                 cur = conn.cursor()
                 cur.execute("update meta set meta_val = ? where meta_key = 'version';",(str(ver_val),))
                 cur.close()
